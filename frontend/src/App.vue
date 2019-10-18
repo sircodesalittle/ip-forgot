d2a19c0fe1db6c564f77b1882fb1ddd7616b4d58
Torey Mercurio <30397914+mtmercurio@users.noreply.github.com>
2019-10-18 16:08:17 -0400

    Added tiles

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,17 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="columns is-multiline">
+    <div v-for="(profile, index) in profiles" :key="index" class="column is-one-fifth">
+      <ProfileCard :profile="profile"/>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ProfileCard from './components/ProfileCard.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    ProfileCard
+  },
+  data: function () {
+    return {
+      profiles: [{name: 'Profile 1', ipAddresses: ['172.31.215.42']}, {name: 'Profile 2', ipAddresses: ['172.31.21.42']},
+        {name: 'Profile 3', ipAddresses: ['255.31.215.42']}, {name: 'Profile 4', ipAddresses: ['172.531.215.42']},
+        {name: 'Profile 5', ipAddresses: ['fe::80']}, {name: 'Profile 6', ipAddresses: ['172.31.123.42']},
+        {name: 'Profile 7', ipAddresses: ['172.31.215.56']}, {name: 'Profile 8', ipAddresses: ['112.31.215.42']},]
+    }
   }
 }
 </script>
---
+++ frontend/src/components/ProfileCard.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="card">
+        <header class="card-header">
+            <p class="card-header-title">
+                {{ profile['name'] }}
+            </p>
+        </header>
+        <div class="card-content">
+            <div class="content">
+                IP Addresses:
+                <ul>
+                    <li v-for="ipAddress in profile['ipAddresses']" :key="ipAddress">
+                        {{ ipAddress }}
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <footer class="card-footer">
+            <a href="#" class="card-footer-item">Load</a>
+            <a href="#" class="card-footer-item">Edit</a>
+            <a href="#" class="card-footer-item">Delete</a>
+        </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ProfileCard',
+    props: {
+        profile: Object,
+    }
+}
+</script>
+
