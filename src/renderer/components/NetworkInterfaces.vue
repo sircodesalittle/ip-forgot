c0b1b0935c2da5d151badee93d8e17a396e6d5ab
Alex Dykstra <alex@alexdykstra.com>
2019-11-25 10:16:02 -0500

    Added save/load config feature

--- src/renderer/components/NetworkInterfaces.vue
+++ src/renderer/components/NetworkInterfaces.vue
@@ -1,26 +1,45 @@
 <template>
-  <div id="wrapper">
-    <main>
-        <div>
-            <ul>
-                <li v-for="(nics, adapterName, index) in networkInterfaces" v-bind:key="index">
-                    {{ adapterName }}
-                    <ul>
-                        <li v-for="(nic, nicIndex) in nics" v-bind:key="nicIndex">
-                            {{ nic.address }}
-                        </li>
-                    </ul>
-                </li>
-            </ul>
-        </div>
-    </main>
-  </div>
+    <div id="wrapper">
+        <main>
+            <h1>Current Adapter Configurations</h1>
+            <div>
+                <ul>
+                    <li v-for="(nics, adapterName, index) in networkInterfaces" v-bind:key="index">
+                        <button v-on:click="saveAdapterConfig($event, adapterName)">Save Adapter Configuration</button>
+                        {{ adapterName }}
+                        <ul>
+                            <li v-for="(nic, nicIndex) in nics" v-bind:key="nicIndex">
+                                {{ nic.address }}
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+
+            <h1>Saved Adapter Configurations</h1>
+            <div>
+                <ul>
+                    <li v-for="(nics, adapterName, index) in savedAdapters" v-bind:key="index">
+                        <button v-on:click="removeSavedAdapterConfig($event, adapterName)">Delete Saved Adapter
+                            Configuration
+                        </button>
+                        {{ adapterName }}
+                        <ul>
+                            <li v-for="(nic, nicIndex) in nics" v-bind:key="nicIndex">
+                                {{ nic.address }}
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </main>
+    </div>
 </template>
 
 <script>
     const os = require('os');
     const EventEmitter = require('events').EventEmitter;
-    import { detailedDiff } from 'deep-object-diff';
+    import {detailedDiff} from 'deep-object-diff';
 
     let networkEventEmitter = new EventEmitter();
     networkEventEmitter.on('added', (event) => {
@@ -33,11 +52,57 @@
         console.log('updated event', event)
     });
 
+    const {exec} = require('child_process');
+
+    /*
+    To dump or export the TCP/IP configuration, use this command:
+        netsh -c interface dump > PATH_AND_FILENAME.txt
+    Use this command to import the TCP/IP configuration:
+        netsh -f PATH_AND_FILENAME.txt
+
+        netsh -c interface ip dump
+        netsh -c interface ipv6 dump
+     */
+    // setTimeout(() => {
+    //     const ls = exec('netsh interface ip add address name="Ethernet 34" address=192.168.168.101 mask=255.255.255.0', function (error, stdout, stderr) {
+    //         if (error) {
+    //             console.log(error.stack);
+    //             console.log('Error code: '+error.code);
+    //             console.log('Signal received: '+error.signal);
+    //         }
+    //         console.log('Child Process STDOUT: '+stdout);
+    //         console.log('Child Process STDERR: '+stderr);
+    //     });
+    //
+    //     ls.on('exit', function (code) {
+    //         console.log('Child process exited with exit code '+code);
+    //     });
+    // }, 5000);
+
+
+    let fs = require('fs');
+    const SAVED_CONFIG_FILENAME = 'savedAdapterConfig.json';
+    const writeSavedAdapterConfig = (json) => {
+        fs.writeFile(SAVED_CONFIG_FILENAME, json, 'utf8', () => console.log('Saved config to disk'));
+    };
+
+    const loadSavedAdapterConfig = (loadedCallback) => {
+        fs.readFile(SAVED_CONFIG_FILENAME, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Failed to load saved config', err);
+            } else {
+                console.log('Loaded saved config from disk');
+                loadedCallback(JSON.parse(data));
+            }
+        });
+    };
+
     export default {
         name: 'network-interfaces',
-        data () {
+        data() {
             return {
-                networkInterfaces: Object.assign({}, os.networkInterfaces())
+                networkInterfaces: Object.assign({}, os.networkInterfaces()),
+                savedAdapters: Object.assign({})
             }
         },
         mounted() {
@@ -52,7 +117,27 @@
                     networkEventEmitter.emit('updated', diffs.updated);
 
                 this.networkInterfaces = Object.assign({}, currentNetworkInterfaces);
-            }, 2000)
+            }, 2000);
+
+            loadSavedAdapterConfig((loadedConfig) => {
+                this.savedAdapters = Object.assign({}, loadedConfig)
+            });
+
+        },
+        methods: {
+            saveAdapterConfig: function (event, adapterName) {
+                let updatedSavedAdapters = Object.assign({}, this.savedAdapters);
+                updatedSavedAdapters[adapterName] = {};
+                updatedSavedAdapters[adapterName] = this.networkInterfaces[adapterName];
+                this.savedAdapters = Object.assign({}, updatedSavedAdapters);
+                writeSavedAdapterConfig(JSON.stringify(this.savedAdapters));
+            },
+            removeSavedAdapterConfig: function (event, adapterName) {
+                let updatedSavedAdapters = Object.assign({}, this.savedAdapters);
+                delete updatedSavedAdapters[adapterName];
+                this.savedAdapters = Object.assign({}, updatedSavedAdapters);
+                writeSavedAdapterConfig(JSON.stringify(this.savedAdapters));
+            }
         }
     }
 </script>
