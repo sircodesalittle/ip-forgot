fb6e65620310a6c52945717a682c6bfb3ceefe1e
Alex Dykstra <alex@alexdykstra.com>
2019-11-26 08:27:21 -0500

    Added ability to set/remove addresses and hooked that functionality into loading saved config

--- src/renderer/components/NetworkInterfaces.vue
+++ src/renderer/components/NetworkInterfaces.vue
@@ -1,6 +1,30 @@
 <template>
     <div id="wrapper">
         <main>
+            <h1>Add Address to Adapter</h1>
+            <select v-model="selectedAddToAdapter">
+                <option disabled value="">Please select one</option>
+                <option v-for="(nics, adapterName, index) in networkInterfaces" v-bind:key="index">
+                    {{ adapterName }}
+                </option>
+            </select>
+            <h2>IPv4</h2>
+            <div>
+                <label for="addAddressInput">Address: </label>
+                <input name="addAddressInput" id="addAddressInput" v-model="addIPv4Address" placeholder="192.168.168.133">
+
+                <label for="addAddressSubnetInput">Subnet: </label>
+                <input name="addAddressSubnetInput" id="addAddressSubnetInput" v-model="addSubnet" placeholder="255.255.255.0">
+            </div>
+            <button v-on:click="executeAddIPv4Address($event)">Add IPv4 Address</button>
+
+            <h2>IPv6</h2>
+            <div>
+                <label for="addIPv6AddressInput">Address: </label>
+                <input name="addIPv6AddressInput" id="addIPv6AddressInput" v-model="addIPv6Address" placeholder="fe80::192:168:168:99/64">
+            </div>
+            <button v-on:click="executeAddIPv6Address($event)">Add IPv6 Address</button>
+
             <h1>Current Adapter Configurations</h1>
             <div>
                 <ul>
@@ -10,6 +34,7 @@
                         <ul>
                             <li v-for="(nic, nicIndex) in nics" v-bind:key="nicIndex">
                                 {{ nic.address }}
+                                <button v-on:click="executeRemoveAddress($event, adapterName, nic)">Remove Address</button>
                             </li>
                         </ul>
                     </li>
@@ -17,6 +42,7 @@
             </div>
 
             <h1>Saved Adapter Configurations</h1>
+            <button v-on:click="applySavedAdapterConfig($event)">Apply Saved Adapter Configuration</button>
             <div>
                 <ul>
                     <li v-for="(nics, adapterName, index) in savedAdapters" v-bind:key="index">
@@ -52,8 +78,6 @@
         console.log('updated event', event)
     });
 
-    const {exec} = require('child_process');
-
     /*
     To dump or export the TCP/IP configuration, use this command:
         netsh -c interface dump > PATH_AND_FILENAME.txt
@@ -62,22 +86,77 @@
 
         netsh -c interface ip dump
         netsh -c interface ipv6 dump
+
+        Add an address to an interface
+            netsh interface ip add address name="Ethernet 34" address=192.168.168.101 mask=255.255.255.0
+        IPv6 version
+            netsh interface ipv6 add address interface="Ethernet 5" address=fd01::172:31:218:39/64
+
+        Remove an address from an interface
+            netsh interface ip delete address "Ethernet 34" addr=192.168.168.101
+            netsh interface ipv6 delete address interface="Local Area Connection 2" address="fd7e:df1d:94d9:0:381d:a3b4:8849:b4bf"
      */
-    // setTimeout(() => {
-    //     const ls = exec('netsh interface ip add address name="Ethernet 34" address=192.168.168.101 mask=255.255.255.0', function (error, stdout, stderr) {
-    //         if (error) {
-    //             console.log(error.stack);
-    //             console.log('Error code: '+error.code);
-    //             console.log('Signal received: '+error.signal);
-    //         }
-    //         console.log('Child Process STDOUT: '+stdout);
-    //         console.log('Child Process STDERR: '+stderr);
-    //     });
-    //
-    //     ls.on('exit', function (code) {
-    //         console.log('Child process exited with exit code '+code);
-    //     });
-    // }, 5000);
+    const {exec} = require('child_process');
+
+    const getIPVersion = (ipVersion) => {
+        let ipVersionCommand = 'ip';
+        if (ipVersion === 6)
+            ipVersionCommand = 'ipv6';
+        return ipVersionCommand;
+    };
+
+    const executeCommand = (command) => {
+        console.debug('Executing command: ' + command);
+        const executedCommand = exec(command, (error, stdout, stderr) => {
+            if (error)
+                console.log(error);
+            // console.log(stdout);
+            // console.log(stderr);
+        });
+
+        executedCommand.on('exit', (code) => {
+            console.debug('Command process exited with exit code: ' + code);
+        })
+    };
+
+    const deleteAdapterAddress = (adapterName, addr, ipVersion=4) => {
+        let ipVersionCommand = getIPVersion(ipVersion);
+        const command = `netsh interface ${ipVersionCommand} delete address "${adapterName}" addr=${addr}`;
+        executeCommand(command);
+    };
+
+    const addAdapterIPv4Address = (adapterName, addr, subnetMask='255.255.255.0') => {
+        let command = `netsh interface ip add address "${adapterName}" address=${addr} mask=${subnetMask}`;
+        executeCommand(command);
+    };
+
+    const addAdapterIPv6Address = (adapterName, cidr) => {
+        const command = `netsh interface ipv6 add address interface="${adapterName}" addr=${cidr}`;
+        executeCommand(command);
+    };
+
+    const applyAdapterConfig = (savedAdapters) => {
+        for (const adapter of Object.keys(savedAdapters)) {
+            let currentAdapterConfig = os.networkInterfaces()[adapter];
+            if (!currentAdapterConfig)
+                throw new Error('Adapter is not available in current adapters. Looking for: ' + adapter);
+            // Iterate over current adapter and remove its IP addresses
+            for (const nic of currentAdapterConfig) {
+                console.log(nic);
+                let ipVersion = 4;
+                if (nic.family === 'IPv6')
+                    ipVersion = 6;
+                deleteAdapterAddress(adapter, nic.address, ipVersion);
+            }
+            // Iterate over saved adapter and add its IP addresses
+            for (const nic of savedAdapters[adapter]) {
+                if (nic.family === 'IPv6')
+                    addAdapterIPv6Address(adapter, nic.cidr);
+                else
+                    addAdapterIPv4Address(adapter, nic.address, nic.netmask);
+            }
+        }
+    };
 
 
     let fs = require('fs');
@@ -102,7 +181,11 @@
         data() {
             return {
                 networkInterfaces: Object.assign({}, os.networkInterfaces()),
-                savedAdapters: Object.assign({})
+                savedAdapters: Object.assign({}),
+                selectedAddToAdapter: '',
+                addIPv4Address: '',
+                addSubnet: '',
+                addIPv6Address: ''
             }
         },
         mounted() {
@@ -137,6 +220,25 @@
                 delete updatedSavedAdapters[adapterName];
                 this.savedAdapters = Object.assign({}, updatedSavedAdapters);
                 writeSavedAdapterConfig(JSON.stringify(this.savedAdapters));
+            },
+            applySavedAdapterConfig: function(event) {
+                console.log('Applying saved adapter config...');
+                applyAdapterConfig(this.savedAdapters);
+            },
+            executeAddIPv4Address: function(event) {
+                addAdapterIPv4Address(this.selectedAddToAdapter, this.addIPv4Address, this.addSubnet);
+                this.addIPv4Address = '';
+                this.addSubnet = '';
+            },
+            executeAddIPv6Address: function(event) {
+                addAdapterIPv6Address(this.selectedAddToAdapter, this.addIPv6Address);
+                this.addIPv6Address = '';
+            },
+            executeRemoveAddress: function($event, adapterName, nic) {
+                let ipVersion = 4;
+                if (nic.family === 'IPv6')
+                    ipVersion = 6;
+                deleteAdapterAddress(adapterName, nic.address, ipVersion);
             }
         }
     }
